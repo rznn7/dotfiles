662cc851e0cf111353e38f6585227bdefc14a3cf
Gabriel Jestin <gjestin@oppscience.com>
2025-04-18 23:58:07 +0200

    add transparency to lazygit floating window

--- nvim/.config/nvim/lua/user/plugin_config/lazygit.lua
+++ nvim/.config/nvim/lua/user/plugin_config/lazygit.lua
@@ -1,5 +1,5 @@
 -- LazyGit setup
-vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+vim.g.lazygit_floating_window_winblend = 1 -- transparency of floating window
 vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
 vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' } -- customize lazygit popup window border characters
 vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
