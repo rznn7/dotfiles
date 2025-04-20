e3af76ccfe4442b6a29e5d4f9fa724e914fbd708
rznn <jest1gabriel@gmail.com>
2025-04-20 16:47:30 +0200

    adjust wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,11 +3,11 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 14
+-- config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrains Mono")
 config.enable_tab_bar = false
 -- config.window_decorations = "RESIZE"
 config.color_scheme = 'rose-pine'
-
-
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.98
 
 return config
