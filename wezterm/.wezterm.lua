adb0f84be370bd15f20d4eae01519320a90ca9d6
rznn <jest1gabriel@gmail.com>
2025-04-23 12:44:33 +0200

    enhance fonts and fallbacks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,10 +3,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 14
--- config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({
+        "JetBrainsMono Nerd Font Mono",
+        "MesloLGS Nerd Font Mono",
+        "Source Han Sans",
+})
 config.enable_tab_bar = false
--- config.window_decorations = "RESIZE"
 config.color_scheme = 'rose-pine'
 config.window_background_opacity = 0.98
 
