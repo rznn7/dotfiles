6dc129921c7d6d06f7b08c9dfa9caad18ea3c3fc
rznn <jest1gabriel@gmail.com>
2025-04-23 20:03:32 +0200

    set default telescope option intellij-like

--- nvim/.config/nvim/lua/user/plugin_config/telescope.lua
+++ nvim/.config/nvim/lua/user/plugin_config/telescope.lua
@@ -1,12 +1,22 @@
--- Global variable for access in keymaps
 _G.telescope_builtin = require('telescope.builtin')
 
 require('telescope').setup({
         defaults = {
+                initial_mode = "normal",
+                layout_strategy = "vertical",
+                sorting_strategy = "ascending",
+                results_title = false,
+                dynamic_preview_title = true,
                 layout_config = {
                         width = 0.95,
                         height = 0.95,
+                        prompt_position = "top",
+                        vertical = {
+                                mirror = true,
+                                preview_height = 0.4
+                        }
                 },
+
         }
 })
 return {}
