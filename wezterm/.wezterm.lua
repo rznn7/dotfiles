642818e1de042668225f3951539e7910475ec088
Gabriel Jestin <gjestin@oppscience.com>
2025-04-25 09:24:28 +0200

    default wezterm to zsh

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.default_prog = { '/usr/bin/zsh' }
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
         "JetBrainsMono Nerd Font Mono",
