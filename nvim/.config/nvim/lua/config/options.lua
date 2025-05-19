cdcf9f0bd6754235b73b3da772f40cec7280bc0a
= <=>
2025-05-19 22:10:07 +0200

    some adjustements

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -17,3 +17,9 @@ vim.lsp.inlay_hint.enable(true)
 
 -- Shell
 vim.o.shell = "zsh"
+
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
