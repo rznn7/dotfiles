73fbe8b0d4d501c1ef8351e4225629a643c28471
Gabriel Jestin <gjestin@oppscience.com>
2025-05-22 17:16:42 +0200

    remove aosp

--- nvim/.config/nvim/lua/plugins/lsp/google-java-format.lua
+++ nvim/.config/nvim/lua/plugins/lsp/google-java-format.lua
@@ -9,9 +9,7 @@ return {
 
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.google_java_format.with({
-            extra_args = { "--aosp" },
-          }),
+          null_ls.builtins.formatting.google_java_format.with({}),
         },
       })
     end,
