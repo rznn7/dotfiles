01f3d69865ff9103d53609726b2be7b1964c900f
= <=>
2025-05-24 11:57:07 +0200

    snacks explorer to the right and file renaming

---
+++ nvim/.config/nvim/lua/plugins/ui/explorer.lua
@@ -0,0 +1,21 @@
+return {
+  "snacks.nvim",
+  opts = {
+    explorer = {
+      enabled = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            preset = "sidebar",
+            preview = true,
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
