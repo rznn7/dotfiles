ed6cf4bd7476d00ad2e1dca6d99d4e8227447e47
= <=>
2025-05-29 14:14:44 +0200

    rebranding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,12 +5,13 @@ local config = wezterm.config_builder()
 config.default_prog = { "/usr/bin/zsh" }
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
+	"SFMono",
 	"JetBrainsMono Nerd Font Mono",
 	"MesloLGS Nerd Font Mono",
 	"Source Han Sans",
 })
 config.enable_tab_bar = false
-config.color_scheme = "rose-pine-dawn"
-config.window_background_opacity = 0.98
+config.color_scheme = "rose-pine-moon"
+-- config.window_background_opacity = 0.98
 
 return config
