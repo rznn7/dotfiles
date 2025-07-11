393cf27bf8fab2800b0df64806be19b03f411e7a
= <=>
2025-07-11 22:26:57 +0200

    adjust blade home screen config

--- i3/.screenlayout/blade_home.sh
+++ i3/.screenlayout/blade_home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1-1 --off --output DP-1-3 --primary --mode 3440x1440 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1-0 --off --output DP-1-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1-1 --off
