1e633768cbda5a1ae7f5a1182c229173bee8b50d
rznn7 <jest1gabriel@gmail.com>
2025-10-12 01:33:29 +0200

    arch fresh install adjustments

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,10 +5,10 @@ local config = wezterm.config_builder()
 config.default_prog = { "/usr/bin/zsh" }
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font Mono",
-	"MesloLGS Nerd Font Mono",
-	"SFMono",
-	"Source Han Sans",
+  "JetBrainsMono Nerd Font Mono",
+  "MesloLGS Nerd Font Mono",
+  "SFMono",
+  "Source Han Sans",
 })
 
 config.enable_tab_bar = false
