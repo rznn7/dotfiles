55185356035f4397d3f24899aeede3412278997f
rznn7 <jest1gabriel@gmail.com>
2025-10-18 17:08:56 +0200

    add dunst and rearrange i3 config

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ fi
 cd "$(dirname "$0")"
 
 # Array of packages to stow
-packages=("nvim" "lazygit" "ghostty" "wezterm" "zsh" "i3" "picom")
+packages=("nvim" "lazygit" "ghostty" "wezterm" "zsh" "i3" "picom" "dunst")
 
 # Create ~/.config directory if it doesn't exist
 mkdir -p ~/.config
