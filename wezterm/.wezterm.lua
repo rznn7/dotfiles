da49e9194ffaabd955852b7891f0f2c96236caef
rznn7 <jest1gabriel@gmail.com>
2025-10-19 16:40:40 +0200

    enhance wezterm display

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,14 +5,21 @@ local config = wezterm.config_builder()
 config.default_prog = { "/usr/bin/zsh" }
 config.font_size = 14
 config.font = wezterm.font_with_fallback({
-  "JetBrainsMono Nerd Font Mono",
-  "MesloLGS Nerd Font Mono",
-  "SFMono",
-  "Source Han Sans",
+	"JetBrainsMono Nerd Font Mono",
+	"MesloLGS Nerd Font Mono",
+	"SFMono",
+	"Source Han Sans",
 })
-
+config.adjust_window_size_when_changing_font_size = false
 config.enable_tab_bar = false
 
 config.color_scheme = "Oldworld"
+
+config.window_padding = {
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
+}
 
 return config
