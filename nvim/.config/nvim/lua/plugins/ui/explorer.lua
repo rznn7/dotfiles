0250c3b5698eeb22c81722b7ca1f65ed683a7b98
Gabriel Jestin <gjestin@oppscience.com>
2025-11-03 11:53:36 +0100

    custom snack explorer

--- nvim/.config/nvim/lua/plugins/ui/explorer.lua
+++ nvim/.config/nvim/lua/plugins/ui/explorer.lua
@@ -5,7 +5,45 @@ return {
       enabled = true,
     },
     picker = {
+      layout = {
+        preset = "vertical",
+      },
       sources = {
+        files = {
+          layout = {
+            preset = "vscode",
+          },
+        },
+        recent = {
+          layout = {
+            preset = "vscode",
+          },
+        },
+        grep = {
+          layout = {
+            preset = "vertical",
+          },
+        },
+        lsp_references = {
+          layout = {
+            preset = "vertical",
+          },
+        },
+        lsp_definitions = {
+          layout = {
+            preset = "vertical",
+          },
+        },
+        buffers = {
+          layout = {
+            preset = "vscode",
+          },
+        },
+        git_files = {
+          layout = {
+            preset = "vscode",
+          },
+        },
         explorer = {
           layout = {
             preset = "sidebar",
