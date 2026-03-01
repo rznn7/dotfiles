d3ec14dd4a4a50387cc6e5ea100c1584b942956a
rznn7 <160644362+rznn7@users.noreply.github.com>
2026-03-01 21:29:41 +0100

    configure sway

---
+++ rofi/.config/rofi/powermenu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+printf 'l  lock\ne  logout\ns  suspend\nh  hibernate\nr  reboot\np  poweroff' \
+    | rofi -dmenu -p "system" \
+        -theme ~/.config/rofi/powermenu.rasi \
+        -kb-custom-1 "l" \
+        -kb-custom-2 "e" \
+        -kb-custom-3 "s" \
+        -kb-custom-4 "h" \
+        -kb-custom-5 "r" \
+        -kb-custom-6 "p"
+
+case $? in
+    10) swaylock -f ;;
+    11) swaymsg exit ;;
+    12) systemctl suspend ;;
+    13) systemctl hibernate ;;
+    14) systemctl reboot ;;
+    15) systemctl poweroff ;;
+esac
