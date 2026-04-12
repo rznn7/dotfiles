a01ab88568d0c944f4f47d91d2a985e71eee7b42
rznn7 <160644362+rznn7@users.noreply.github.com>
2026-04-12 22:17:03 +0200

    fix sway power menu

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
-printf 'l  lock\ne  logout\ns  suspend\nh  hibernate\nr  reboot\np  poweroff' \
-    | rofi -dmenu -p "system" \
-        -theme ~/.config/rofi/powermenu.rasi \
-        -kb-custom-1 "l" \
-        -kb-custom-2 "e" \
-        -kb-custom-3 "s" \
-        -kb-custom-4 "h" \
-        -kb-custom-5 "r" \
-        -kb-custom-6 "p"
+choice=$(printf 'l  lock\ne  logout\ns  suspend\nh  hibernate\nr  reboot\np  poweroff' |
+  rofi -dmenu -p "system" \
+    -theme ~/.config/rofi/powermenu.rasi \
+    -kb-custom-1 "l" \
+    -kb-custom-2 "e" \
+    -kb-custom-3 "s" \
+    -kb-custom-4 "h" \
+    -kb-custom-5 "r" \
+    -kb-custom-6 "p")
 
-case $? in
-    10) swaylock -f ;;
-    11) swaymsg exit ;;
-    12) systemctl suspend ;;
-    13) systemctl hibernate ;;
-    14) systemctl reboot ;;
-    15) systemctl poweroff ;;
+ret=$?
+
+case $ret in
+0)
+  case "$choice" in
+  l*) swaylock -f ;;
+  e*) swaymsg exit ;;
+  s*) systemctl suspend ;;
+  h*) systemctl hibernate ;;
+  r*) systemctl reboot ;;
+  p*) systemctl poweroff ;;
+  esac
+  ;;
+10) swaylock -f ;;
+11) swaymsg exit ;;
+12) systemctl suspend ;;
+13) systemctl hibernate ;;
+14) systemctl reboot ;;
+15) systemctl poweroff ;;
 esac
