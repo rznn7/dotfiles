8e68868724d455aa14156a2898a1aa8558748a2c
rznn7 <160644362+rznn7@users.noreply.github.com>
2026-06-08 10:49:11 +0200

    better diff checking

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -4,6 +4,7 @@ return {
   keys = {
     { "<leader>gl", "<cmd>DiffviewFileHistory<cr>", desc = "Git log (repo)" },
     { "<leader>gL", "<cmd>DiffviewFileHistory %<cr>", desc = "Git log (file)" },
+    { "<leader>gr", "<cmd>DiffviewOpen<cr>", desc = "Git review (working tree)" },
     { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
   },
 }
